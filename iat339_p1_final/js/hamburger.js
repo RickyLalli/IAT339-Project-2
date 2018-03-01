9e5fd7c486f925730658c66184dc124ca2c03696
Unknown <rlalli@sfu.ca>
2018-03-01 00:37:41 -0800

    html validator fixes

--- iat339_p1_final/js/hamburger.js
+++ iat339_p1_final/js/hamburger.js
@@ -10,3 +10,19 @@ function myFunction() {
         x.className = "nav1";
     }
 }
+
+// Get the container element
+var btnContainer = document.getElementById("nav1");
+
+// Get all buttons with class="btn" inside the container
+var btns = document.getElementsByClassName("navAnchor");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("activeNav");
+    current[0].className = current[0].className.replace(" activeNav", "");
+    this.className += " activeNav";
+  });
+  console.log("forloop");
+}
